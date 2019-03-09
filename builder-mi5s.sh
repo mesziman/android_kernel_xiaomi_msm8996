6bb4520f27cd93a7268a3d3c12536492d7969d85
Gabor <meszaros.gabor.10k@gmail.com>
2019-03-09 19:00:12 +0100

    good builder

--- builder-mi5s.sh
+++ builder-mi5s.sh
@@ -3,7 +3,8 @@
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
 CCACHEDIR=../CCACHE/capricorn
-TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+#TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+TOOLCHAINDIR=/pipeline/build/root/toolchain/jonas/linaro_gcc/aarch64-linux-gnu-7.4.1-2019.02
 TOOLCHAIN32=/pipeline/build/root/toolchain/arm-linux-androideabi-4.9
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Syberia"
@@ -27,18 +28,22 @@ export KBUILD_BUILD_HOST="github"
 #export CC=/pipeline/build/root/toolchain/dtc/bin/clang
 #export CC=/pipeline/build/root/toolchain/gclang/clang-r349610/bin
 #export CXX=/pipeline/build/root/toolchain/dtc/bin/clang++
-#export CC=clang
-#export CXX=clang++
+export CC=clang
+export CXX=clang++
 #export cc-name=clang
-#export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
-#export CROSS_COMPILE=aarch64-linux-android-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+#export CROSS_COMPILE=aarch64-linux-android-
+#export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 #export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
 export USE_CCACHE=1
 export CCACHE_DIR=$CCACHEDIR/.ccache
 
 echo "which cl $(which clang)"
+echo "which 32tc $(which ${CROSS_COMPILE_ARM32}ld))"
+echo "which ${CROSS_COMPILE_ARM32}gcc"
+echo "realpath of 32tc $(realpath $(dir $(which ${CROSS_COMPILE_ARM32}ld))/..)"
 make clean && make mrproper
 make O=out -C $KERNEL_DIR capriszar_defconfig
 make O=out -C $KERNEL_DIR  -j$( nproc --all )
@@ -50,7 +55,10 @@ if [ $? != 0 ]; then
   echo "FAILED BUILD"
 fi
 }
-
+echo "======================VERIFY CLANG==============================="
+cat $KERNEL_DIR/out/include/generated/compile.h
+echo "======================VERIFY CLANG==============================="
 cd $ANYKERNEL_DIR/capricorn
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+cp $FINAL_ZIP ${WERCKER_REPORT_ARTIFACTS_DIR}/
 mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
