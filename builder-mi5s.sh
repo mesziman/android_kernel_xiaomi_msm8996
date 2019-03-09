e59dcff99cdd61cc6f14bb23415182cd8c5074fa
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2019-03-09 18:41:16 +0100

    update wercker to latest

---
+++ builder-mi5s.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+KERNEL_DIR=$PWD
+ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel2
+CCACHEDIR=../CCACHE/capricorn
+TOOLCHAINDIR=/pipeline/build/root/toolchain/aarch64-linux-android-4.9
+TOOLCHAIN32=/pipeline/build/root/toolchain/arm-linux-androideabi-4.9
+DATE=$(date +"%d%m%Y")
+KERNEL_NAME="Syberia"
+DEVICE="-capricorn-"
+VER="-0.1"
+TYPE="PIE-EAS"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
+
+rm $ANYKERNEL_DIR/capricorn/Image.gz-dtb
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+PATH="${PATH}:${TOOLCHAINDIR}/bin:${TOOLCHAIN32}/bin:/pipeline/build/root/toolchain/gclang/clang-r349610/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAINDIR}/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAINDIR}/aarch64-linux-android/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAINDIR}/aarch64-linux-android/lib64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAIN32}/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAIN32}/arm-linux-androideabi/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/pipeline/build/root/toolchain/gclang/clang-r349610/lib64"
+export ARCH=arm64
+export KBUILD_BUILD_USER="mesziman"
+export KBUILD_BUILD_HOST="github"
+#export CC=/pipeline/build/root/toolchain/dtc/bin/clang
+#export CC=/pipeline/build/root/toolchain/gclang/clang-r349610/bin
+#export CXX=/pipeline/build/root/toolchain/dtc/bin/clang++
+#export CC=clang
+#export CXX=clang++
+#export cc-name=clang
+#export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$TOOLCHAINDIR/bin/aarch64-linux-android-
+#export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+#export LD_LIBRARY_PATH=$TOOLCHAINDIR/lib/
+export USE_CCACHE=1
+export CCACHE_DIR=$CCACHEDIR/.ccache
+
+echo "which cl $(which clang)"
+echo "which 32tc $(which ${CROSS_COMPILE_ARM32}ld))"
+echo "realpath of 32tc $(realpath $(dir $(which ${CROSS_COMPILE_ARM32}ld))/..)"
+make clean && make mrproper
+make O=out -C $KERNEL_DIR capriszar_defconfig
+make O=out -C $KERNEL_DIR  -j$( nproc --all )
+
+{
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/capricorn
+} || {
+if [ $? != 0 ]; then
+  echo "FAILED BUILD"
+fi
+}
+
+cd $ANYKERNEL_DIR/capricorn
+zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
+mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
