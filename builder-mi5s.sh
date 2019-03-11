04cfa5fab65ee8e7b978e54a19f9c1b5c51ed15a
Your Name <you@example.com>
2019-03-11 11:20:50 +0000

    use Snapdragon 6.0.9

--- builder-mi5s.sh
+++ builder-mi5s.sh
@@ -55,7 +55,7 @@ echo "===================WHICH========================="
 make clean && make mrproper
 make O=out -C $KERNEL_DIR capriszar_defconfig
 
-make O=out -C $KERNEL_DIR  -j$( nproc --all ) CC=clang  CLANG_TRIPLE=aarch64-linux-gnu- \
+make O=out -C $KERNEL_DIR  -j$( nproc --all ) ARCH=arm64 CC=clang  CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=aarch64-linux-android-
 
 {
